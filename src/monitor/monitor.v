ba92af155a607a3c902e2d00abddcdb92c7ecf07
Xavier Mitault <xavier.mitault@epitech.eu>
2024-10-20 00:53:15 +0100

    Remove location thing

--- src/monitor/monitor.v
+++ src/monitor/monitor.v
@@ -16,8 +16,8 @@ pub mut:
 }
 
 fn Monitor.new(config_file string, postgres_uri string) !Monitor {
-	config := parse_config(config_file) or { return error('${@LOCATION} :: ${err.msg()}') }
-	mut db := pg.connect_with_conninfo(postgres_uri) or { return error('${@LOCATION} :: ${err.msg()}') }
+	config := parse_config(config_file) or { return error('parse_config :: ${err.msg()}') }
+	mut db := pg.connect_with_conninfo(postgres_uri) or { return error('connect_with_conninfo :: ${err.msg()}') }
 	mut mon := Monitor{
 		db: db
 	}
@@ -27,7 +27,7 @@ fn Monitor.new(config_file string, postgres_uri string) !Monitor {
 		create table FileWatcher
 	} or {
 		mon.db.close()
-		return error('${@LOCATION} :: ${err.msg()}')
+		return error('mon.db create table monitor :: ${err.msg()}')
 	}
 	config_to_db(config, mut mon)
 	for item in mon.command_watcher {
@@ -78,8 +78,8 @@ fn (mut mon Monitor) wait() int {
 }
 
 // todo: get exit value
-pub fn monitor(config_file string, internal_db_path string) {
-	mut mon := Monitor.new(config_file, internal_db_path) or {
+pub fn monitor(config_file string, postgres_uri string) {
+	mut mon := Monitor.new(config_file, postgres_uri) or {
 		eprintln(err.msg())
 		return
 	}
